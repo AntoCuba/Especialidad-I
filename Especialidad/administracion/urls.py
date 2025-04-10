83409f2f40119ef9376f6844bcf7458072869dc5
Benjamin Osses Bagatello <benjaminosses20@gmail.com>
2025-04-09 20:33:56 -0400

    fix: Se movio layout a carpeta templates, borrando core adicional.

--- Especialidad/administracion/urls.py
+++ Especialidad/administracion/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns=[
     path('usuarios/', views.usuario_view, name='usuarios'),
-    path('/historial/', views.historial_actividades, name='historial_actividades'),
+    path('historial/', views.historial_actividades, name='historial_actividades'),
     path('/', views.dashboard, name='dashboard'),
 ]
