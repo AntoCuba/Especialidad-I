4cdd876a4913c5bb022ba3852f36f51ad0797d3b
Antonia C. Orellana <anto.cuba.2002@gmail.com>
2025-04-16 18:33:12 -0400

    Se corrigio el modelo de usuario, se le pasaron los usuarios a la vista de django, se crearon los formularios para crear usuario

--- Especialidad/administracion/views.py
+++ Especialidad/administracion/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Usuario
 
 # Create your views here.
 
 def usuario_view(request):
-    return render(request, 'usuarios.html')
+    usuarios = Usuario.objects.all()
+    return render(request, 'usuarios.html', {'usuarios': usuarios} )
 
 def historial_actividades(request):
     return render(request, 'historial_actividades.html')
