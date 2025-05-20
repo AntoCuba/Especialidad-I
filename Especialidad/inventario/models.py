73ba7409dc8635d0a586e7e8dee808afcf86c900
Benjamin Osses Bagatello <benjaminosses20@gmail.com>
2025-05-19 23:46:47 -0400

    fix: Se arregló tallas y cantidades + Productos con bajo stock

--- Especialidad/inventario/models.py
+++ Especialidad/inventario/models.py
@@ -4,10 +4,8 @@ class Producto(models.Model):
     nombre_producto = models.CharField(max_length=100)
     numero_orden = models.CharField(max_length=50, blank=True, null=True)
     valor_producto_unidad = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    talla = models.CharField(max_length=10)
     numero_tracking = models.CharField(max_length=100, blank=True, null=True)
     proveedor = models.CharField(max_length=100, blank=True, null=True)
-    cantidad = models.IntegerField(blank=True, null=True)
     precio_venta = models.DecimalField(max_digits=10, decimal_places=2)
     boleta_factura = models.FileField(upload_to='boletas_facturas/', blank=True, null=True)
 
@@ -15,4 +13,18 @@ class Producto(models.Model):
         return self.nombre_producto
 
     def sin_stock(self):
-        return not self.cantidad or self.cantidad == 0 #Devuelve true si el stock es 0 y false en caso contrario.
+        return not self.productotalla_set.filter(cantidad__gt=0).exists()
+
+    def get_tallas_cantidades_str(self):
+        parts = []
+        for pt in self.productotalla_set.all():
+            parts.append(f"{pt.talla} ({pt.cantidad})")
+        return " ; ".join(parts)
+
+class ProductoTalla(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    talla = models.CharField(max_length=10)
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.producto.nombre_producto} - Talla: {self.talla} - Cantidad: {self.cantidad}"
