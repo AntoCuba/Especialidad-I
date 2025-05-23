10ba05198171cd83f91442fa9a476bbc9a5be17d
Benjamin Osses Bagatello <benjaminosses20@gmail.com>
2025-05-23 18:25:59 -0400

    fix: Se arregló Listar Proveedores y texto Sidebar

--- Especialidad/proveedores/models.py
+++ Especialidad/proveedores/models.py
@@ -3,9 +3,10 @@ from django.db import models
 class Proveedor(models.Model):
 
     nombre = models.CharField(max_length=100)
-    producto = models.CharField(max_length=20)
+    producto = models.CharField(max_length=50)
     autenticacion = models.BooleanField(default=False)
     tiempo_envio = models.CharField(max_length=50)
+    descripcion = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nombre
